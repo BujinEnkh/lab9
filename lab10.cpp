8328ab90bd5bb297c6cf3abbcd9acb1dbd276c9e
Anar008 <160026492+Anar008@users.noreply.github.com>
2025-11-17 22:27:01 +0800

    comment oruulav

--- lab10.cpp
+++ lab10.cpp
@@ -1,91 +1,86 @@
-#include <iostream>
-#include <cstring>
-#include <algorithm>
+#include <iostream>    // оролт/гаралт
+#include <cstring>     // char array үйлдэл
+#include <algorithm>   // sort ашиглана
 #include <vector>
-#include <cstdlib>
-#include <ctime>
+#include <cstdlib>     // rand, srand
+#include <ctime>       // time()
 using namespace std;
 
-// Template class definition
+// Template List class
 template <typename T>
 class List {
 private:
-    T *elements;
-    int capacity;
-    int currentSize;
+    T *elements;       // динамик array
+    int capacity;      // одоогийн багтаамж
+    int currentSize;   // одоогийн хэмжээ
 
-    void resize(int newCapacity) {
-        T *newArray = new T[newCapacity];
+    void resize(int newCapacity) {      // хэмжээ өсгөх функц
+        T *newArray = new T[newCapacity];   // шинээр array авах
         for (int i = 0; i < currentSize; ++i) {
-            newArray[i] = elements[i];
+            newArray[i] = elements[i];      // хуучин элементүүдийг шинэ array-д хуулна
         }
-        delete[] elements;
-        elements = newArray;
-        capacity = newCapacity;
+        delete[] elements;          // хуучныг устгана
+        elements = newArray;        // шинэ array-г заана
+        capacity = newCapacity;     // шинэ багтаамж хадгална
     }
 
 public:
-    List() : elements(nullptr), capacity(0), currentSize(0) {}
-    ~List() { delete[] elements; }
+    List() : elements(nullptr), capacity(0), currentSize(0) {}  // байгуулагч функц
+    ~List() { delete[] elements; }   // устгагч функц
 
-    T *begin() { return elements; }
-    T *end() { return &elements[currentSize]; }
+    T *begin() { return elements; }          // эхлэл pointer
+    T *end() { return &elements[currentSize]; }   // төгсгөл pointer 
 
-    void add(const T &element) {
-        if (currentSize == capacity) {
-            int newCapacity = (capacity == 0) ? 1 : capacity * 2;
+    void add(const T &element) {     // нэмэх функц
+        if (currentSize == capacity) {   // дүүрсэн бол
+            int newCapacity = (capacity == 0) ? 1 : capacity * 2;  // багтаамж 0-тэй тэнцүү бол 1, үгүй бол 2 дахин томруулна
             resize(newCapacity);
         }
-        elements[currentSize++] = element;
+        elements[currentSize++] = element;    // элемент нэмээд size өсгөнө
     }
 
-    int size() const { return currentSize; }
+    int size() const { return currentSize; }   // одоогийн хэмжээ
 
-    T &operator[](int index) {
-        if (index >= currentSize) {
-            throw out_of_range("Index out of range");
-        }
+    T &operator[](int index) {      // аль элементирүү индексаар хандахыг харуулах
+        if (index >= currentSize) throw out_of_range("Index out of range");
         return elements[index];
     }
 
-    void remove(int index) {
-        if (index >= currentSize) {
-            throw out_of_range("Index out of range");
-        }
+    void remove(int index) {         // устгах
+        if (index >= currentSize) throw out_of_range("Index out of range");
         for (int i = index; i < currentSize - 1; i++) {
-            elements[i] = elements[i + 1];
+            elements[i] = elements[i + 1];     // элементүүдийг урагш шилжүүлэх
         }
-        currentSize--;
+        currentSize--;       // size бууруулах
     }
 
-    void insert(const T &element, int index) {
+    void insert(const T &element, int index) {   // оруулах
         if (currentSize == capacity) {
             int newCapacity = (capacity == 0) ? 1 : capacity * 2;
             resize(newCapacity);
         }
-        currentSize++;
+        currentSize++;        // size нэмэх
         for (int i = currentSize; i > index; i--) {
-            elements[i] = elements[i - 1];
+            elements[i] = elements[i - 1];      // арын элементүүдийг зөөх
         }
-        elements[index] = element;
+        elements[index] = element;              // шинэ элементийг байрлуулах
     }
 };
 
 class shape {
 protected:
-    char *name;
+    char *name;         // дүрсний нэр
 public:
     shape(const char *n) {
-        name = new char[strlen(n) + 1];
-        strcpy(name, n);
+        name = new char[strlen(n) + 1];     // нэрт зай авах
+        strcpy(name, n);                     // нэр хуулах
     }
 
-    virtual ~shape() {
-    }
+    virtual ~shape() { }    // виртуал destructor
 
-    virtual float calculate_area() const = 0;
+    virtual float calculate_area() const = 0;    // талбай тооцох жинхэнэ хийсвэр функц 
 
-    virtual void show() const {
+    virtual void show() const {         // хэвлэх функц
         cout << "Shape: " << name << endl;
     }
 };
@@ -98,13 +93,11 @@ public:
     _2Dshape(const char *n) : shape(n) {}
     virtual ~_2Dshape() {}
 
-    float calculate_area() const override {
-        return 0;
-    }
+    float calculate_area() const override { return 0; }
 
     void show() const override {
         shape::show();
-        cout << "Area: " << calculate_area() << endl;
+        cout << "Area: " << calculate_area() << endl;   // талбай хэвлэх
     }
 };
 
@@ -166,33 +159,29 @@ bool compareByArea(shape *a, shape *b) {
 int main() {
     srand(time(0));
 
-    List<_2Dshape *> a;
+    List<_2Dshape *> a;   // shape pointer-үүдийн лист
 
-    for (int i = 0; i < 20; i++) {
-        int k = (rand() % 3) + 1;
-        float c = rand() % 20;
+    for (int i = 0; i < 20; i++) {        // 20 shape үүсгэнэ
+        int k = (rand() % 3) + 1;         // 1–3 төрөл
+        float c = rand() % 20;            // санамсаргүй хэмжээ
 
-        if (k == 1) {
-            a.add(new Circle("Circle", c));
-        } else if (k == 2) {
-            a.add(new Triangle("Triangle", c));
-        } else if (k == 3) {
-            a.add(new Square("Square", c));
-        }
+        if (k == 1) a.add(new Circle("Circle", c));
+        else if (k == 2) a.add(new Triangle("Triangle", c));
+        else if (k == 3) a.add(new Square("Square", c));
 
-        a[a.size() - 1]->calculate_area();
+        a[a.size() - 1]->calculate_area(); // талбайг дуудаж авна
     }
 
     cout << "Unsorted:\n";
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 20; i++) {      // эрэмблээгүй хэвлэх
         a[i]->show();
         cout << "---------------------------\n";
     }
 
-    sort(a.begin(), a.end(), compareByArea);
+    sort(a.begin(), a.end(), compareByArea);  // талбайгаар эрэмблэх
 
-    cout << "Sorted:\n";
-    for (int i = 0; i < 20; i++) {
+    cout << "\nSorted:\n";
+    for (int i = 0; i < 20; i++) {      // эрэмбэлсэн хувилбар хэвлэх
         a[i]->show();
         cout << "---------------------------\n";
     }
